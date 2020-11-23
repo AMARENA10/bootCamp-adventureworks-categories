1edfe46c5a77e08bd1aa7d897d53b397e1e91c49
AMARENA10 <marena125al@gmail.com>
2020-11-23 18:05:33 -0500

    primer proyecto

---
+++ src/main/java/bootCamp/adventureworkscategories/AdventureworksCategoriesApplication.java
@@ -0,0 +1,13 @@
+package bootCamp.adventureworkscategories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureworksCategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureworksCategoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootCamp/adventureworkscategories/AdventureworksCategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package bootCamp.adventureworkscategories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureworksCategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
